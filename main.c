7681f4113bdd9663e0879eefc6fc92defc460a68
AbihuAndrade <113129486+AbihuAndrade@users.noreply.github.com>
2023-01-28 22:12:54 -0600

    Main final

--- main.c
+++ main.c
@@ -1,33 +1,38 @@
 #include "lib/include.h"
 
-int main(void) {
+int main(void)
+{
 
     volatile uint16_t vdig[6];
 
     int k=0;
     Configurar_SysTick();
-    Configurar_PLL();  //Confiuracion de velocidad de reloj
+    Configurar_PLL();
+
+    //Se configura la velocidad del reloj
     Configura_Reg_ADC0();
     Configurar_UART1();
-     config_interrupt();
+    
   
     char num[4];
     //printString("3");
-    while(1) {
+    while(1)
+    {
 
-         ADC0->PSSI |= (1<<2)| (1<<1);  
+         ADC0->PSSI |= (1<<2)| (1<<1)|(1<<3);  
              /* Enable SS3 conversion or start sampling data from AN0 */
-         while((ADC0->RIS & (1<<2)) == 0){} ;   /* Wait untill sample conversion completed*/
-         vdig[0]= ADC0->SSFIFO2& 0xFFF;
-         
-          /* Wait untill sample conversion completed*/
-         
-         vdig[1]= ADC0->SSFIFO2 &0xFFF;
-         
-         vdig[2]= ADC0->SSFIFO2&0xFFF;
-         
+         while((ADC0->RIS & (1<<3)) == 0){} ;   /* Wait untill sample conversion completed*/
+         vdig[0]= ADC0->SSFIFO3& 0xFFF;
          utoa(vdig[0],num,10);
          transmisor(num);
+          /* Wait untill sample conversion completed*/
+          while((ADC0->RIS & (1<<1)) == 0){} 
+         vdig[1]= ADC0->SSFIFO1 &0xFFF;
+         
+         vdig[2]= ADC0->SSFIFO1&0xFFF;
+         
+         //uso del utoa, que transforma de un unsigned integer a string
+         
         
          utoa(vdig[1],num,10);
          transmisor(num);
@@ -36,9 +41,9 @@ int main(void) {
                                       /* Enable SS3 conversion or start sampling data from AN0 */
            /* Wait untill sample conversion completed*/
          
-         while((ADC0->RIS & (1<<1)) == 0){} ; 
+         while((ADC0->RIS & (1<<2)) == 0){} ; 
            /* Wait untill sample conversion completed*/
-         vdig[3]= ADC0->SSFIFO1& 0xFFF0;
+         vdig[3]= ADC0->SSFIFO2& 0xFFF0;
          
          
          
@@ -47,10 +52,10 @@ int main(void) {
           
                                     /* Enable SS3 conversion or start sampling data from AN0 */
             /* Wait untill sample conversion completed*/
-         vdig[4]= ADC0->SSFIFO1& 0xFF0F;
+         vdig[4]= ADC0->SSFIFO2& 0xFF0F;
             /* Wait untill sample conversion completed*/
          
-         vdig[5]= ADC0->SSFIFO1& 0xFFFF;
+         vdig[5]= ADC0->SSFIFO2& 0xFFFF;
         
         utoa(vdig[3],num,10);
          transmisor(num);
@@ -59,24 +64,12 @@ int main(void) {
         
          utoa(vdig[5],num,10);
          transmisor(num); 
-          ADC0->ISC = (1<<2)|(1<<1);
+          ADC0->ISC = (1<<2)|(1<<1)|(1<<3);
 
           /* read adc coversion result from SS3 FIFO*/
-        ADC0->ISC = 8;          /* clear coversion clear flag bit*/
+                  /* clear coversion clear flag bit*/
 			/*control Green PF3->LED */
 		
     }
 
-}
-
-void GPIOJ_Handler(void) {
-  if (GPIOJ_AHB->RIS & 0x01){
-
-  }
-}
-
-void IntDefaultHandler(void){
-  if (GPIOJ_AHB->RIS & 0x01){
-
-  }
 }
